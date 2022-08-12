6b93b5f62e7a5652e0b39639daae82107258573a
dk <jsdk__@aliyun.com>
2022-08-12 16:04:26 +0800

    feat: 11 Handling Update and Delete Requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,24 +1,17 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
+  Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 @Controller('coffees')
 export class CoffeesController {
-  // @Get()
-  // findAll() {
-  //   return 'This action return all coffees';
-  // }
-
   @Get()
-  findAll(@Res() response) {
-    // 此处可以使用express原生方法（默认express），但会因不同底层库(express、fastify) 有不同的API,简易尽可能的使用nest标准方法
-    response.status(200).send('This action return all coffees');
+  findAll() {
+    return 'This action return all coffees';
   }
 
   @Get(':id')
@@ -27,8 +20,17 @@ export class CoffeesController {
   }
 
   @Post()
-  @HttpCode(HttpStatus.GONE) // 设置HttpStatus为410
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffees`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffees`;
   }
 }
