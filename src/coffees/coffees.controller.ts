69b574814cf4c95d1215b1e0be19d5ef29de774d
dk <jsdk__@aliyun.com>
2022-08-12 15:56:28 +0800

    feat: 10 Response Status Codes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,33 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 @Controller('coffees')
 export class CoffeesController {
+  // @Get()
+  // findAll() {
+  //   return 'This action return all coffees';
+  // }
+
   @Get()
-  findAll() {
-    return 'This action return all coffees';
+  findAll(@Res() response) {
+    // 此处可以使用express原生方法（默认express），但会因不同底层库(express、fastify) 有不同的API,简易尽可能的使用nest标准方法
+    response.status(200).send('This action return all coffees');
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    console.log(id);
     return `This action return #${id} coffees`;
   }
 
   @Post()
+  @HttpCode(HttpStatus.GONE) // 设置HttpStatus为410
   create(@Body() body) {
     return body;
   }
