ba8dec1bf0c8203e113951769b1c7429cd3267e1
dk <jsdk__@aliyun.com>
2022-08-14 21:23:20 +0800

    feat: 18 Handling Malicious Request Data

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 白名单，去除DTO以外的无效属性
+      forbidNonWhitelisted: true, // 让DTO以外的无效属性抛出异常
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
