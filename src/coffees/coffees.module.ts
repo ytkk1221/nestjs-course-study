dfddca43c43643054405e41ba524b9dfcda2e9c2
dk <jsdk__@aliyun.com>
2022-08-16 10:17:23 +0800

    feat: 37 Nonclassbased Provider Tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,10 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 导入实体
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: COFFEE_BRANDS,
+      useValue: ['雀巢', '星巴克'],
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
