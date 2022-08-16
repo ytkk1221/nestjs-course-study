c35844d456c0c411f8ffe7066f1ad5a1b13a8d9f
dk <jsdk__@aliyun.com>
2022-08-16 10:38:03 +0800

    feat: 40 Leverage Async Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,26 +6,31 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
+import { Connection } from 'typeorm';
 
-@Injectable()
-export class CoffeeBrandsFactory {
-  create() {
-    /* ... do something ... */
-    return ['雀巢', '星巴克'];
-  }
-}
+// @Injectable()
+// export class CoffeeBrandsFactory {
+//   create() {
+//     /* ... do something ... */
+//     return ['雀巢', '星巴克'];
+//   }
+// }
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 导入实体
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
+    // CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
-        brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeeBrands = await connection.query('select * ...');
+        // 模拟异步
+        const coffeeBrands = await Promise.resolve(['星巴克', '瑞幸']);
+        return coffeeBrands;
+      },
+      inject: [Connection],
     },
   ],
   exports: [CoffeesService],
