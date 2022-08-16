6be4f219b02df711f9df53da8cd935039d56d28c
dk <jsdk__@aliyun.com>
2022-08-16 16:10:42 +0800

    feat: 44 Introducing the Config Module【05 Application Configuration】

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,19 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
-      type: 'mysql', // 数据库类型
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'db_nest_study', // 数据库名
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE, // 数据库名
       autoLoadEntities: true, // 自动加载实体
       synchronize: true, // 同步数据库,可以使@Entity() 装饰器的类自动生成SQL表以及数据等，生产环境需要关闭
     }),
