4c1655eca0b13be7f5fe40be222d0a33b982c3c1
dk <jsdk__@aliyun.com>
2022-08-17 10:39:29 +0800

    feat: 53 Catch Exceptions with Filters

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,29 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter<T extends HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const request = ctx.getRequest<Request>();
+    const exceptionResponse = exception.getResponse();
+    const error =
+      typeof response === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as object);
+    response.status(status).json({
+      ...error,
+      path: request.url,
+      timestamp: new Date().toString(),
+    });
+  }
+}
