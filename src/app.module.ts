26fea1f044916c0503922ba3329ff368ecd60906
dk <jsdk__@aliyun.com>
2022-08-17 09:48:56 +0800

    feat: 50 Asynchronously Configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,15 +20,17 @@ import appConfig from './config/app.config';
       // }),
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DATABASE, // 数据库名
-      autoLoadEntities: true, // 自动加载实体
-      synchronize: true, // 同步数据库,可以使@Entity() 装饰器的类自动生成SQL表以及数据等，生产环境需要关闭
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DATABASE, // 数据库名
+        autoLoadEntities: true, // 自动加载实体
+        synchronize: true, // 同步数据库,可以使@Entity() 装饰器的类自动生成SQL表以及数据等，生产环境需要关闭
+      }),
     }),
     CoffeeRatingModule,
     DatabaseModule,
