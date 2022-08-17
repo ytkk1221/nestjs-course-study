ff0e2ddd4a75301c914c54c8178b7a6ba0ee84f5
dk <jsdk__@aliyun.com>
2022-08-17 16:15:53 +0800

    feat: 59 Request-response time

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { WrapResponseInterceptor } from './interceptors/wrap-response.interceptor';
 import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
+import { LoggingMiddleware } from './middleware/logging.middleware';
+
 /*守卫和过滤器都放入common.module中*/
 @Module({
   imports: [ConfigModule],
@@ -15,4 +22,12 @@ import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
     { provide: APP_INTERCEPTOR, useClass: TimeoutInterceptor },
   ],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    // consumer
+    //   .apply(LoggingMiddleware)
+    //   .exclude()
+    //   .forRoutes({ path: 'coffees', method: RequestMethod.GET });
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
