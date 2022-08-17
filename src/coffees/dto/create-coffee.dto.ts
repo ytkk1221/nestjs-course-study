71a2b16acd4ad47f6cfb57133140ec4d19b5cfa7
dk <jsdk__@aliyun.com>
2022-08-17 17:11:45 +0800

    feat: 63 Decorating Model Properties

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,10 +1,16 @@
-import { IsString } from 'class-validator'; // 提供验证dto验证规则，自动响应为400
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'; // 提供验证dto验证规则，自动响应为400
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: '咖啡名' })
   @IsString()
   readonly name: string;
+
+  @ApiProperty({ description: '咖啡品牌' })
   @IsString()
   readonly brand: string;
+
+  @ApiProperty({ example: [] })
   @IsString({ each: true })
   readonly flavors: string[];
 }
