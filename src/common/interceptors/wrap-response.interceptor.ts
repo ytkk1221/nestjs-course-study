29436f289a32be8a16c995bae403da6cc848a1f3
dk <jsdk__@aliyun.com>
2022-08-17 11:38:14 +0800

    feat: 56 Add Pointcuts with Interceptors

---
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -0,0 +1,15 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('before...');
+    return next.handle().pipe(map((data) => ({ data })));
+  }
+}
